0936f7521d9273410c1d6646f218e101747bc0cf
Daniël <152068787+Tripplepoeh@users.noreply.github.com>
2025-03-29 18:12:59 +0100

    rfid via SPI

--- Test/stm32/i2c_arduino/slave.ino
+++ Test/stm32/i2c_arduino/slave.ino
@@ -1,6 +1,9 @@
 #include <Wire.h> // Include wire library voor I2C
 #include <Servo.h> // Include servo library voor servo
 
+#include <SPI.h> // Include SPI library voor RFID
+#include <MFRC522.h> // Include MFRC522 library voor RFID
+
 #define SLAVE_ADDRESS 0x08 // Defineer slave adres
 #define SDA PB7 // Defineer SDA pin
 #define SCL PB6 // Defineer SCL pin
@@ -8,6 +11,13 @@
 #define SERVO_PIN PB8 // Defineer servo pin
 #define BUTTON_PIN PB9 // Defineer button pin
 #define RFID_PIN PB10 // Defineer RFID pin
+
+// SPI RFID dingen (Pins nog even checken)
+#define SS_PIN PB11 // Defineer SS pin voor RFID
+#define RST_PIN PB12 // Defineer RST pin voor RFID
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Maak een RFID object aan met SS en RST pinnen
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 Servo deurServo; // Defineer deur servo 
 
@@ -23,6 +33,10 @@ void setup() {
 Wire.setSDA(SDA); // Set SDA pin
 Wire.setSCL(SCL); // Set SCL pin
 Serial.begin(115200); // Start serial communicatie
+
+SPI.begin(); // Start SPI communicatie
+rfid.PCD_Init(); // Initialiseer RFID reader
+Serial.println("RFID reader initialized"); // Print dat RFID reader is geinitialiseerd
 
 Wire.begin(SLAVE_ADDRESS); // Start I2C communicatie met slave adres
 Wire.onReceive(ActionRec); // Roep receiveEvent aan wanneer er data ontvangen wordt
@@ -52,11 +66,28 @@ if (rfidState != lastRfidState && rfidState == LOW) {
     }
 }
 lastRfidState = rfidState; // Update lastRfidState
-
-
-
-
 }
+
+void readRfid() {
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) { // Als er een nieuwe kaart is en deze is gelezen
+        Serial.print("RFID adres: "); // Print RFID adres
+        for (int i = 0; i < rfid.uid.size; i++) { // Loop door RFID adres
+            Serial.print(rfid.uid.uidByte[i], HEX); // Print RFID adres in hex
+        }
+        Serial.println(); // Print newline
+        rfid.PICC_HaltA(); // Stop RFID reader
+    }
+}
+
+void checkcorrectRFID() {
+    if (rfid.PICC_ReadCardSerial == rfidAdres) { // Als RFID adres gelijk is aan het adres van de sleutel
+        Serial.println("RFID adres correct"); // Print dat RFID adres correct is
+        openDeur(); // Open deur
+    } else {
+        Serial.println("RFID adres niet correct"); // Print dat RFID adres niet correct is
+    }
+}
+
 void ActionRec(int numBytes) {
     if(Wire.available()) {
         uint8_t adres = Wire.read(); // Lees actie van master
