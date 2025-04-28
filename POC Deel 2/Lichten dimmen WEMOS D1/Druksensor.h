1215343e77c29a001e72bdff85c5dcf34370df12
BitOcker <144342250+BitOcker@users.noreply.github.com>
2025-04-28 13:18:09 +0200

    Update Druksensor.h

--- POC Deel 2/Lichten dimmen WEMOS D1/Druksensor.h
+++ POC Deel 2/Lichten dimmen WEMOS D1/Druksensor.h
@@ -3,14 +3,25 @@
 
 #include <Arduino.h>
 
+// Definitie van de Druksensor-klasse.
+// Deze klasse wordt gebruikt om een druksensor te beheren en detecteren
+// of een persoon wordt gedetecteerd met behulp van een analoge waarde.
 class Druksensor {
 public:
-    Druksensor(uint8_t sensorPin);  
-    void begin();                   
-    bool isPersonDetected();         
+    // Constructor: Initialiseert de druksensor met een specifieke pin.
+    // sensorPin: De pin waarop de druksensor is aangesloten.
+    Druksensor(uint8_t sensorPin);
+
+    // Initialiseer de druksensor.
+    // Stelt de pin in als invoer (INPUT) zodat analoge waarden kunnen worden gelezen.
+    void begin();
+
+    // Controleer of een persoon wordt gedetecteerd.
+    // @return true als de sensor een waarde boven de drempel detecteert, anders false.
+    bool isPersonDetected();
 
 private:
-    uint8_t _sensorPin;              
+    uint8_t _sensorPin; // Pin waarop de druksensor is aangesloten.
 };
 
 #endif // DRUKSENSOR_H
