b763e641f761fb9cc7354079a24d6fea1cfc66e3
BitOcker <144342250+BitOcker@users.noreply.github.com>
2025-04-28 13:22:08 +0200

    Update Main.ino

--- POC Deel 2/Lichten dimmen WEMOS D1/Main.ino
+++ POC Deel 2/Lichten dimmen WEMOS D1/Main.ino
@@ -1,16 +1,21 @@
-#include <Arduino.h>
-#include "LEDstrip.h"
+#include <Arduino.h>       // Arduino-bibliotheek voor algemene functies
+#include "LEDstrip.h"      // Headerbestand voor het beheren van de LED-strip
 
-#define LED_PIN D0
-#define SENSOR_PIN A0
-#define NUM_LEDS 9
+// Definitie van constante parameters
+#define LED_PIN D0         // Pin waarop de LED-strip is aangesloten
+#define SENSOR_PIN A0      // Pin waarop de druksensor is aangesloten
+#define NUM_LEDS 9         // Aantal LEDs in de LED-strip
 
+// Maak een instance van de BedLightController-klasse
+// Hiermee wordt de LED-strip en druksensor beheerd.
 BedLightController bedLightController(LED_PIN, NUM_LEDS, SENSOR_PIN);
 
 void setup() {
+  // Initialiseer de LED-strip en druksensor.
   bedLightController.begin();
 }
 
 void loop() {
+  // Voer voortdurend de logica uit om de LED-strip en druksensorstatus te beheren.
   bedLightController.update();
 }
