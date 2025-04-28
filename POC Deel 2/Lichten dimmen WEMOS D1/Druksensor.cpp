c69d3aff87897c995273e883653779279370f13d
BitOcker <144342250+BitOcker@users.noreply.github.com>
2025-04-28 16:34:18 +0200

    Update Druksensor.cpp

--- POC Deel 2/Lichten dimmen WEMOS D1/Druksensor.cpp
+++ POC Deel 2/Lichten dimmen WEMOS D1/Druksensor.cpp
@@ -1,21 +1,13 @@
 #include "Druksensor.h"
 
-// Constructor voor de Druksensor-klasse.
-// Initialiseert de drukgevoelige sensor met de opgegeven pin.
-Druksensor::Druksensor(uint8_t sensorPin)
-    : _sensorPin(sensorPin) {
+Druksensor::Druksensor(uint8_t pin) : _pin(pin) {
+    pinMode(_pin, INPUT);
 }
 
-// Initialiseer de sensor.
-// Stel de opgegeven pin in als een invoer (INPUT) voor het lezen van analoge waarden.
-void Druksensor::begin() {
-    pinMode(_sensorPin, INPUT);
+bool Druksensor::isPressed() {
+    return analogRead(_pin) >= 30; // Drempelwaarde voor druk
 }
 
-// Controleer of een persoon is gedetecteerd door de druksensor.
-// Leest de analoge waarde van de sensor en vergelijkt deze met een drempelwaarde.
-// Als de waarde gelijk aan of hoger dan 30 is, wordt aangenomen dat een persoon is gedetecteerd.
-bool Druksensor::isPersonDetected() {
-    int sensorValue = analogRead(_sensorPin);
-    return sensorValue >= 30; // Drempelwaarde om te detecteren
+int Druksensor::getValue() {
+    return analogRead(_pin);
 }
