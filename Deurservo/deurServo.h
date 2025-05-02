b5a7f49f873fbf9e5c2a7f460e1d68718583c3f1
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2025-05-02 19:36:58 +0200

    Update deurServo.h

--- Deurservo/deurServo.h
+++ Deurservo/deurServo.h
@@ -12,20 +12,19 @@
 extern "C" {
 #include "stm32l4xx_hal.h"
 }
-#define STAPPEN 100
+#define STAPPEN 100 
 
 class deurServo {
 private:
-	int beweging;
-	TIM_HandleTypeDef* pwmkanaal;
+	int beweging; // variabele die checkt of de servo al aan het bewegen is.
+	TIM_HandleTypeDef* pwmkanaal; // variabele waarin de timer wordt opgeslagen.
 
 public:
 	deurServo(TIM_HandleTypeDef* timer);
 	virtual ~deurServo();
-	void draaiServo(int begin, int eind, int tijd);
-	void setPWM(int pulsebreedte);
-	uint32_t pwmNaarHoek(int hoek);
-
+	void draaiServo(int begin, int eind, int tijd); // draait de servo van beginhoek naar eindhoek in de gegeven tijd in milisec.
+	void setPWM(int pulsebreedte); // set de CCR waarde
+	uint32_t pwmNaarHoek(int hoek); // berekent de hoek om de CCR waarde te setten
 };
 
 #endif /* SRC_DEURSERVO_H_ */
