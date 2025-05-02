928359efda585978b96e82431d73199d9a3a52bd
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2025-05-02 19:30:57 +0200

    Create deurServo.h

---
+++ Deurservo/deurServo.h
@@ -0,0 +1,31 @@
+/*
+ * deurServo.h
+ *
+ *  Created on: Apr 25, 2025
+ *      Author: basti
+ */
+
+#ifndef SRC_DEURSERVO_H_
+#define SRC_DEURSERVO_H_
+#include <stdio.h>
+#include <stdint.h>
+extern "C" {
+#include "stm32l4xx_hal.h"
+}
+#define STAPPEN 100
+
+class deurServo {
+private:
+	int beweging;
+	TIM_HandleTypeDef* pwmkanaal;
+
+public:
+	deurServo(TIM_HandleTypeDef* timer);
+	virtual ~deurServo();
+	void draaiServo(int begin, int eind, int tijd);
+	void setPWM(int pulsebreedte);
+	uint32_t pwmNaarHoek(int hoek);
+
+};
+
+#endif /* SRC_DEURSERVO_H_ */
