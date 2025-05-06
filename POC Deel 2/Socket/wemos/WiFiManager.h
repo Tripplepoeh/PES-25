e1ebcb9a0846aac355c17841f5bdcbfa2804df59
aasepaas <146937344+aasepaas@users.noreply.github.com>
2025-05-06 15:07:45 +0200

    Create WiFiManager.h

---
+++ POC Deel 2/Socket/wemos/WiFiManager.h
@@ -0,0 +1,22 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+#include <ESP8266WiFi.h>
+
+class WiFiManager {
+private:
+    const char* ssid;
+    const char* password;
+    const char* serverIP;
+    int serverPort;
+    WiFiClient client;
+
+public:
+    WiFiManager(const char* ssid, const char* password, const char* serverIP, int serverPort);
+    void wifiInit();
+    void connectToServer();
+    void sendData(const char* data);
+    char* receiveData();
+};
+
+#endif
