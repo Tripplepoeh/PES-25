6e3ab7ce810592743775d1e4572cc4139b95c2fb
aasepaas <146937344+aasepaas@users.noreply.github.com>
2025-05-07 14:22:22 +0200

    Update deurServo.h

--- Deurservo/deurServo.h
+++ Deurservo/deurServo.h
@@ -12,19 +12,29 @@
 extern "C" {
 #include "stm32l4xx_hal.h"
 }
-#define STAPPEN 100 
+#define STAPPEN 100
+
+class RFIDSensor; // verbeter naam als nodig
+class deurknop;
 
 class deurServo {
 private:
-	int beweging; // variabele die checkt of de servo al aan het bewegen is.
-	TIM_HandleTypeDef* pwmkanaal; // variabele waarin de timer wordt opgeslagen.
+	int beweging;
+	TIM_HandleTypeDef* pwmkanaal;
+	uint32_t tickaantal;
+	RFIDSensor* sensor;
+	deurknop* knop;
 
 public:
 	deurServo(TIM_HandleTypeDef* timer);
 	virtual ~deurServo();
-	void draaiServo(int begin, int eind, int tijd); // draait de servo van beginhoek naar eindhoek in de gegeven tijd in milisec.
-	void setPWM(int pulsebreedte); // set de CCR waarde
-	uint32_t pwmNaarHoek(int hoek); // berekent de hoek om de CCR waarde te setten
+	void draaiServo(int begin, int eind, int tijd);
+	void setPWM(int pulsebreedte);
+	void getRFID(RFIDSensor* sens);
+	void getKnop(deurknop* knop);
+	void checkOpen();
+	uint32_t pwmNaarHoek(int hoek);
+
 };
 
 #endif /* SRC_DEURSERVO_H_ */
