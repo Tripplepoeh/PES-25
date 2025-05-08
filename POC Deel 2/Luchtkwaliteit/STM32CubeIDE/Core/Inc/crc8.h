2c712e72e209550ac36eaf29000019553b9f3cf1
Daniël <152068787+Tripplepoeh@users.noreply.github.com>
2025-05-08 14:04:35 +0200

    STM32CubeIDE - Luchtkwaliteit getest en afgerond

---
+++ POC Deel 2/Luchtkwaliteit/STM32CubeIDE/Core/Inc/crc8.h
@@ -0,0 +1,15 @@
+/*
+ * crc8.h
+ *
+ *  Created on: May 8, 2025
+ *      Author: danie
+ */
+
+#ifndef CRC8_H
+#define CRC8_H
+
+#include <stdint.h>
+
+uint8_t sensiron_crc8(const uint8_t *data, uint16_t count);
+
+#endif // CRC8_H
---
+++ POC Deel 2/Luchtkwaliteit/STM32CubeIDE/Core/Src/crc8.c
@@ -0,0 +1,20 @@
+/*
+ * crc8.c
+ *
+ *  Created on: May 8, 2025
+ *      Author: danie
+ */
+
+#include "crc8.h"
+
+uint8_t sensiron_crc8(const uint8_t *data, uint16_t count) {
+    uint8_t crc = 0xFF;
+    for (uint16_t i = 0; i < count; i++) {
+        crc ^= data[i];
+        for (uint8_t bit = 0; bit < 8; bit++) {
+            if (crc & 0x80) crc = (crc << 1) ^ 0x31;
+            else           crc <<= 1;
+        }
+    }
+    return crc;
+}
