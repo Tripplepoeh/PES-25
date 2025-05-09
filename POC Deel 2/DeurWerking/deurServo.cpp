3abb22ac8562004311bb70fe6c141330a0a8eaab
aasepaas <146937344+aasepaas@users.noreply.github.com>
2025-05-09 12:24:18 +0200

    Create deurServo.cpp

---
+++ POC Deel 2/DeurWerking/deurServo.cpp
@@ -0,0 +1,70 @@
+/*
+ * deurServo.cpp
+ *
+ *  Created on: Apr 25, 2025
+ *      Author: basti
+ */
+
+#include <RFIDSensor.hpp> // verbeter naam als nodig
+#include "deurServo.h"
+#include "deurknop.h"
+
+deurServo::deurServo(TIM_HandleTypeDef* timer) {
+	pwmkanaal = timer;
+	beweging = false;
+
+
+}
+
+deurServo::~deurServo() {
+	// TODO Auto-generated destructor stub
+}
+void deurServo::getRFID(RFIDSensor* sens) {
+	sensor = sens;
+}
+
+void deurServo::getKnop(deurknop* k) {
+	knop = k;
+}
+void deurServo::checkOpen(){
+	if (!(knop->statusopen())) {
+		draaiServo(0, 180, 3000);
+		draaiServo(180, 0, 3000);
+
+	}
+	else if (sensor->CheckKaart())  {
+		uint8_t* rfidWaarde = sensor->RFIDSensorWaarde();
+		if (sensor->CheckToegang()){
+		draaiServo(0, 180, 3000);
+		draaiServo(180, 0, 3000);
+		}
+	}
+}
+
+void deurServo::setPWM(int pulsbreedte){
+	tickaantal = pulsbreedte;
+	__HAL_TIM_SET_COMPARE(pwmkanaal, TIM_CHANNEL_1, tickaantal);
+}
+uint32_t deurServo::pwmNaarHoek(int hoek) {
+	return 500 + (hoek * 2000 / 180);
+}
+
+void deurServo::draaiServo(int begin, int eind, int tijd){
+	if (begin == eind) return;
+	if (!beweging) {
+		beweging = true;
+	int staprust = tijd / STAPPEN;
+		for (int i = 0; i < STAPPEN; i++) {
+				int hoekhuidig = begin + ((eind - begin) * i) / STAPPEN;
+				uint32_t pwmwaarde = pwmNaarHoek(hoekhuidig);
+				setPWM(pwmwaarde);
+				HAL_Delay(staprust);
+		}
+		uint32_t pwmwaarde = pwmNaarHoek(eind);
+		setPWM(pwmwaarde);
+
+	}
+
+	beweging = false;
+	HAL_Delay(3000);
+}
