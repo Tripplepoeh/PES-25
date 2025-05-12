c43ac97ccc5caa57b57dc6e2846c434272c6f3ff
Thomas <33629077+thomaswitvliet@users.noreply.github.com>
2025-05-12 17:17:17 +0200

    Add files via upload

---
+++ POC Deel 2/Noodknop/EmergencyButton.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+
+// Singleton-klasse voor de noodknop
+class EmergencyButton {
+public:
+    // Retourneert de enige instantie van de knop
+    static EmergencyButton& getInstance() {
+        static EmergencyButton instance;
+        return instance;
+    }
+
+    // Geeft aan of de knop is ingedrukt
+    bool isPressed() const {
+        return pressed;
+    }
+
+    // Drukt de knop in (zet status op true)
+    void press() {
+        pressed = true;
+        std::cout << "[NOODKNOP] Ingedrukt\n";
+    }
+
+    // Resetten van de knopstatus (handmatig of na afhandeling)
+    void reset() {
+        pressed = false;
+        std::cout << "[NOODKNOP] Gereset\n";
+    }
+
+    // Verhindert kopieren van de singleton
+    EmergencyButton(const EmergencyButton&) = delete;
+    EmergencyButton& operator=(const EmergencyButton&) = delete;
+
+private:
+    // Prive constructor voor Singleton-patroon
+    EmergencyButton() : pressed(false) {}
+
+    bool pressed; // Interne status van de knop
+};
