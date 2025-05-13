06453192cb31402ff2cba8a100f8561377d2baab
aasepaas <146937344+aasepaas@users.noreply.github.com>
2025-05-13 19:22:35 +0200

    Update grondvochtigheidSensor.h

--- POC Deel 2/grondvochtigheidsensor/grondvochtigheidSensor.h
+++ POC Deel 2/grondvochtigheidsensor/grondvochtigheidSensor.h
@@ -10,13 +10,10 @@ class grondvochtigheidSensor {
     grondvochtigheidSensor();
     ~grondvochtigheidSensor();
     void leesSensor(); //functie die de grondvochtigheidsensor meet en print
-    int natOfDroog(); //functie die teruggeeft of de grond droog of nat is. 0 is te nat, 1 is te droog en 2 is perfect
+    int getWaarde(); //functie die teruggeeft of de grond droog of nat is. 0 is te nat, 1 is te droog en 2 is perfect
 
   private:
-   int grondWaarde; //opslag grondvochtigheidsensor waarde
-   int droog = 400; //drempel wanneer de grond te droog is. boven dit is de grond te droog
-   int nat = 270; // //drempel wanneer de grond te nat is. onder dit is de grond te nat
-   
+   int grondWaarde; //opslag grondvochtigheidsensor waarde   
 };
 
 #endif //GRONDVOCHTIGHEIDSENSOR_H
