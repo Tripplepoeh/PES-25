a36e0ea68690c6c44661c73983e3fa78585ab68c
aasepaas <146937344+aasepaas@users.noreply.github.com>
2025-05-30 00:10:31 +0200

    Update i2cSlave.cpp

--- POC Deel 2/i2c code master en slave/slave/i2cSlave.cpp
+++ POC Deel 2/i2c code master en slave/slave/i2cSlave.cpp
@@ -9,12 +9,12 @@
 #include "i2cSlave.h"
 #include <stdio.h>
 #include <string.h>
-#include "i2cBeheer.hpp"
+#include "i2cBeheer.h"
 
-//Voeg objecten toe die gebruikt worden in de code 
+//Voeg objecten toe die gebruikt worden in de code
 extern UART_HandleTypeDef huart2;
 extern I2C_HandleTypeDef hi2c1;
-extern I2CBeheer i2c; 
+extern I2CBeheer i2c;
 //Grootte van binnenkomende en uitgaande berichten
 #define RX_BUF_SIZE 32
 #define TX_BUF_SIZE 32
@@ -34,7 +34,7 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c,
                           uint16_t AddrMatchCode)
 {
 	if (hi2c->Instance == I2C1) { //Ga hier alleen in wanneer het gaat om de juiste i2c lijn
-		//Debug tekst 
+		//Debug tekst
         char buf[32];
         int len = snprintf(buf, sizeof(buf),
                            "ADDR MATCH at 0x%02X %s\r\n",
@@ -69,13 +69,13 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c,
 
 
 void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) //slave ontvang code
-{//Verwerk de resterende bytes van de master 
+{//Verwerk de resterende bytes van de master
     if (hi2c->Instance == I2C1) {
 	if (rxcount < RX_BUF_SIZE - 1) {
 	    rxcount++;
 	    if(rxcount == RX_BUF_SIZE - 1){
 	    	//Ontvangt de laatste byte als de grootte van de bytes 1 kleiner is dan de grootte
-	        HAL_I2C_Slave_Sequential_Receive_IT(hi2c, rx_buf+rxcount, 1, I2C_LAST_FRAME); 
+	        HAL_I2C_Slave_Sequential_Receive_IT(hi2c, rx_buf+rxcount, 1, I2C_LAST_FRAME);
 	    } else {
 	    	//Ontvangt de bytes als volgende frame anders
 	        HAL_I2C_Slave_Sequential_Receive_IT(hi2c, rx_buf+rxcount, 1, I2C_NEXT_FRAME);
@@ -108,7 +108,7 @@ void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c)
 {
     if (hi2c->Instance == I2C1) {
         if (rxcount > 0) {
-            ProcessReceivedData1();  // proces de data als er data is ontvangen 
+            ProcessReceivedData1();  // proces de data als er data is ontvangen
         }
         HAL_I2C_EnableListen_IT(hi2c); //Zet luister modus weer aan na het bericht
     }
@@ -133,6 +133,7 @@ void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c) //error met i2c
 
 void ProcessReceivedData1() {
 	//Debug tekst
+	char msg[512];
 	 for (uint16_t i = 0; i < rxcount; i++)
 	    {
 	        int len = snprintf(msg, sizeof(msg), "  byte %2u = 0x%02X\r\n", i, rx_buf[i]);
