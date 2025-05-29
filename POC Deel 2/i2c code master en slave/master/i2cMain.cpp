6a73fcd20754def6cc91a5cb27c84a9ed9065e49
aasepaas <146937344+aasepaas@users.noreply.github.com>
2025-05-30 00:11:39 +0200

    Update i2cMain.cpp

--- POC Deel 2/i2c code master en slave/master/i2cMain.cpp
+++ POC Deel 2/i2c code master en slave/master/i2cMain.cpp
@@ -2,19 +2,25 @@
 #include <iostream>
 #include <vector>
 #include <unistd.h>  // voor usleep
+#include "statuscontrol.h"
+#include "logtodatabase.h"
 
 int main() {
-    I2CMaster i2c;
+    logToDatabase log("localhost", "rasp_b", "rasp_b", "sensordata");
+    statuscontrole str(&log);
+    I2CMaster controller(&str);
 
-    std::vector<int> slaveAddresses = { 0x12 };
+    std::vector<int> slaveAddresses = { 0x12, 0x20, 0x28 };
 
-    if (!i2c.init(slaveAddresses)) {
+    if (!controller.init(slaveAddresses)) {
         std::cerr << "I2C initialisatie mislukt!\n";
         return 1;
     }
 
+    std::cout << "Start polling loop...\n";
+
     while (true) {
-        i2c.pollSlaves();
+        controller.pollSlaves();
         usleep(10000);  // 10ms delay
     }
 
