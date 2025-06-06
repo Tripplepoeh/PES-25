7961929398e4da86ae3f3e09982efa5ebe112a06
BitOcker <144342250+BitOcker@users.noreply.github.com>
2025-06-06 23:43:47 +0200

    Update Bewegingssensor.h

--- POC Deel 2/Bewegingssensor/Core/Inc/Bewegingssensor.h
+++ POC Deel 2/Bewegingssensor/Core/Inc/Bewegingssensor.h
@@ -12,16 +12,14 @@
 
 class Bewegingssensor {
 public:
-	Bewegingssensor(GPIO_TypeDef* gpiopPort, uint16_t gpioPin, I2C_HandleTypeDef* i2cHandle);
+	Bewegingssensor();
 
 //	void controleerEnVerzend();
 	void CheckWaarde();
 	uint8_t GetWaarde() const;
 
 private:
-	GPIO_TypeDef* _gpioPort;
-	uint16_t _gpioPin;
-	I2C_HandleTypeDef* _i2c;
+
 	uint8_t _waarde;
 
 };
