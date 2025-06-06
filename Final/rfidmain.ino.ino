1c83f72daf1c1faa664d27708ed56564c3ecda2e
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2025-06-06 21:02:59 +0200

    Update rfidmain.ino.ino

--- Final/rfidmain.ino.ino
+++ Final/rfidmain.ino.ino
@@ -5,10 +5,11 @@
 #define RFID_SS_PIN D8
 #define RFID_RST_PIN D3
 
-const char* WIFI_SSID = "NSELab";  // SSID van het WiFi-netwerk
+const char* WIFI_SSID = "NSELab";          // SSID van het WiFi-netwerk
 const char* WIFI_PASSWORD = "NSELabWiFi";  // Wachtwoord voor het WiFi-netwerk
 const char* SERVER_IP = "145.52.127.184";  // IP-adres van de server waarmee gecommuniceerd wordt
-const int SERVER_PORT = 12345;  // Poortnummer van de server
+const int SERVER_PORT = 12345;             // Poortnummer van de server
+unsigned long laatstescan = 0;
 
 
 RFIDSensor rfid(RFID_SS_PIN, RFID_RST_PIN);
@@ -16,44 +17,49 @@ RFIDSensor rfid(RFID_SS_PIN, RFID_RST_PIN);
 WiFiManager wifi(WIFI_SSID, WIFI_PASSWORD, SERVER_IP, SERVER_PORT);
 
 void setup() {
-    Serial.begin(9600);
-    rfid.init();
-    wifi.wifiInit();
+  Serial.begin(9600);
+  rfid.init(); //! begin de scanner
+  wifi.wifiInit(); //! maak connectie met de wifi
+  laatstescan = millis(); //! timer om sensor reset bij te houden
 }
 
 void loop() {
-  wifi.connectToServer();
-    if (rfid.checkCard()) {
-        uint8_t* uid = rfid.getUID();
-        uint8_t size = 5;
+  char sender[64];
+/*! kijk of er een kaar aangeboden wordt. zo ja, pak byte voor byte de uid en zet deze om naar hexadecimaal.
+stuur daarna door naar de server. als er niks wordt gevonden stuur 0. */
+  if (rfid.checkKaart()) {
+    laatstescan = millis();
+   
+    uint8_t* uid = rfid.getUID();
+    uint8_t grootte = 5;
 
-        // Convert UID to string format (hex)
-        char uidStr[32];
-        char* uidsend;
-        for (int i = 0; i < size; ++i) {
-            sprintf(&uidStr[i * 2], "%02X", uid[i]);
-        }
-
-        char sender[64];
-        uint64_t HtoI = strtoull(uidStr, nullptr, 16);
-        snprintf(sender, sizeof(sender), "set rfidsensor %llu", (uint64_t)HtoI);
-        Serial.println(sender);
-        
-
-        // Send to server
-        wifi.sendData(sender);
-
-        // Wait for response from server
-        char* response = wifi.receiveData();
-        Serial.println(response);
-
-        delay(2000); 
+    char uidStr[32];
+    for (int i = 0; i < grootte; ++i) {
+      sprintf(&uidStr[i * 2], "%02X", uid[i]);
     }
-    else {
-      char sender2[64];
-      snprintf(sender2, sizeof(sender2), "set rfidsensor %d", 0);
-      wifi.sendData(sender2);
-      delay(100);
+
+
+    uint64_t HtoI = strtoull(uidStr, nullptr, 16);
+    snprintf(sender, sizeof(sender), "set rfidsensor %llu", (uint64_t)HtoI);
+    Serial.println(sender);
+
+    char* response = wifi.receiveData();
+    Serial.println(response);
+    
+
+    delay(250);
+  } else {
+    snprintf(sender, sizeof(sender), "set rfidsensor %d", 0);
+    
+    delay(100);
+  }
+  if (millis() - lastScanTime > 10000) { //! als er 10 seconden niks wordt gezien, reinitialize de rfidsensor om lock-up te voorkomen.
+        Serial.println("Reinitializing RFID");
+      
+      rfid.init(); 
+      lastScanTime = millis();
     }
-    wifi.disconnectFromServer();
+  wifi.connectToServer(); //! verbind met de zerver
+  wifi.sendData(sender); //! stuur de data;
+  wifi.disconnectFromServer(); //! verbreek de verbinding
 }
