98c808a655d03da56f0a1f7c4ef570fc50213d69
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2025-06-06 20:56:51 +0200

    Update RFIDSensor.hpp

--- Final/RFIDSensor.hpp
+++ Final/RFIDSensor.hpp
@@ -8,15 +8,15 @@ class RFIDSensor {
 public:
     RFIDSensor(uint8_t ssPin, uint8_t rstPin);
     void init();
-    bool checkCard();
+    bool checkKaart();
     uint8_t* getUID();
-    uint8_t getUIDSize();
 
 private:
     MFRC522 mfrc522;
     uint8_t uid[5];
     uint8_t uidBuffer[10];   
-    uint8_t uidSize;
+    int teller = 0;
+
 };
 
 #endif
