ef9511a39f6b0cdbb995b8893b5ddc790fd06b83
aasepaas <146937344+aasepaas@users.noreply.github.com>
2025-06-14 21:33:27 +0200

    Update logtodatabase.h

--- Eindproduct/Pi_B (Wired i2c)/I2C Master code/logtodatabase.h
+++ Eindproduct/Pi_B (Wired i2c)/I2C Master code/logtodatabase.h
@@ -8,11 +8,15 @@ class logToDatabase : public IDatabase{
 public:
 	logToDatabase();
 	~logToDatabase();
+//Stuur alle belangrijke informatie van de database door zodat deze klasse toegang heeft 
 	void dbInit(const std::string &host, const std::string& user, const std::string& password, const std::string& db);
+//Functie waarmee sensorwaardes gelogd worden
 	void logSensorWaarde(std::string naam, double waarde);
 	
 private:
+//MYSQL database object waarmee met de database gewerkt kan worden met code
 MYSQL* database;
+//Bool die aangeeft of de connectie met de database klopt 
 bool verbonden;
 	
 };
